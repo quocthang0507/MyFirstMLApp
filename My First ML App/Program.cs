c957dd24677839a5a2ecf7eccea6fa5ff55cae50
La Quốc Thắng <quocthang0507@gmail.com>
2020-08-15 15:42:37 +0700

    Add project files.

---
+++ My First ML App/Program.cs
@@ -0,0 +1,20 @@
+﻿using My_First_ML_AppML.Model;
+using System;
+
+namespace My_First_ML_App
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Add input data
+			var input = new ModelInput();
+			//input.SentimentText = "That is rude"; // TOXIC : TRUE
+			input.SentimentText = "I need some help"; // TOXIC : FALSE
+
+			// Load model and predict output of sample data
+			ModelOutput result = ConsumeModel.Predict(input);
+			Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+		}
+	}
+}
